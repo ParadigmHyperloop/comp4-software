701d71c8cafd463a307ddd1033f96cedb68f2eb7
Liam <liamwaghorn@gmail.com>
2019-01-21 14:22:10 -0500

    Remove not needed networking includes from main

--- OnPod/FlightComputer/src/FlightComputer.cpp
+++ OnPod/FlightComputer/src/FlightComputer.cpp
@@ -2,14 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-
 #include "FlightComputer/PodInternalNetwork.h"
 
 struct pod {
