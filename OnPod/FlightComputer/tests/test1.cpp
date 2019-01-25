1c48fe6d7e088c1eb01eb6cfb9bec6019a3b8710
Liam <liamwaghorn@gmail.com>
2019-01-25 10:51:15 -0500

    Get make file working with project functions
    Added macros that add all src files so we dont have to keep that part updated. Added library include paths for project

--- OnPod/FlightComputer/tests/test1.cpp
+++ OnPod/FlightComputer/tests/test1.cpp
@@ -15,9 +15,3 @@ TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]") {
   REQUIRE(Factorial(3) == 6);
   REQUIRE(Factorial(10) == 3628800);
 }
-
-// Compile & run:
-// - g++ -std=c++11 -Wall -I$(CATCH_SINGLE_INCLUDE) -o 010-TestCase
-// 010-TestCase.cpp && 010-TestCase --success
-// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% 010-TestCase.cpp && 010-TestCase
-// --success
