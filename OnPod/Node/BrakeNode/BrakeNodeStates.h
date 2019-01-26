ecf7427ae75815f96225ab86e4cbf11ecb248c8f
Jonathan Taylor <jttaylor@mun.ca>
2019-01-26 15:01:44 -0330

    Started BrakeNode class

---
+++ OnPod/Node/BrakeNode/BrakeNodeStates.h
@@ -0,0 +1,17 @@
+enum State {
+    BOOT,
+    STANDBY,
+    ARMING,
+    ARMED,
+    FLIGHT,
+    BRAKING,
+    VENTING,
+    RETRIEVAL,
+    ERROR
+}
+
+class BrakeNodeState {
+    
+    private State state_;
+    
+}
