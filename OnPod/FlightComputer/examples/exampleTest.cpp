e6adfcf7317505c80cbd337b1fd48f492f8b01cb
Unknown <liamwaghorn@gmail.com>
2019-01-26 13:14:59 -0500

    Update exampleTest.cpp

--- OnPod/FlightComputer/examples/exampleTest.cpp
+++ OnPod/FlightComputer/examples/exampleTest.cpp
@@ -1,7 +1,6 @@
 #include "catch.hpp"
 
 int Factorial(int number) {
-  // return number <= 1 ? number : Factorial(number - 1) * number;  // fail
   return number <= 1 ? 1 : Factorial(number - 1) * number;  // pass
 }
 
