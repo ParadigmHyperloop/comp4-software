2cf8d00d891e5bb3a4a05e28db90f06561c84ca6
Unknown <liamwaghorn@gmail.com>
2019-01-26 19:10:00 -0500

    Update Makefile

--- OnPod/FlightComputer/Debug/sources.mk
+++ OnPod/FlightComputer/Debug/sources.mk
@@ -6,6 +6,8 @@ C_UPPER_SRCS :=
 CXX_SRCS := 
 C++_SRCS := 
 OBJ_SRCS := 
+INO_SRCS := 
+PDE_SRCS := 
 CC_SRCS := 
 ASM_SRCS := 
 CPP_SRCS := 
@@ -18,6 +20,8 @@ EXECUTABLES :=
 C_UPPER_DEPS := 
 CXX_DEPS := 
 OBJS := 
+INO_DEPS := 
+PDE_DEPS := 
 CPP_DEPS := 
 C_DEPS := 
 
