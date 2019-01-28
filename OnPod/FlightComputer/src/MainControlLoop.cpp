3634cdfb4df55c55361362a26c7a9ff9bbe505ca
Unknown <liamwaghorn@gmail.com>
2019-01-27 19:01:46 -0500

    Possible Solution for unexpected node states - FC

--- OnPod/FlightComputer/src/MainControlLoop.cpp
+++ OnPod/FlightComputer/src/MainControlLoop.cpp
@@ -1,6 +1,10 @@
 
 #include "FlightComputer/structs.h"
 
+/**
+ * The flags array should be all zeros if all systems are nominal.
+ * Each index represents a different subsystem.
+ */
 bool checkFlags(PodValues* podVals){
 	for(int i = 0 ; i < podVals->flagSize ; i++){
 		if(podVals->flagsArray[i] == 1){
@@ -10,9 +14,15 @@ bool checkFlags(PodValues* podVals){
 	return true;
 }
 
-bool standbyHold(PodValues* podVals){
-	if(checkFlags(podVals)){
-		return true;
+/**
+ * Checks that the current state of the break node is within
+ * one of its expected states.
+ */
+bool nominalStates(BreakNodeStates bnState, BreakNodeStates bnAccepted[],int bnAcceptedSize){
+	for(int i = 0 ; i < bnAcceptedSize; i++){
+		if(bnAccepted[i] == bnState){
+			return true;
+		}
 	}
 	return false;
 }
@@ -21,9 +31,9 @@ bool standbyToArming(PodValues* podVals){
 
 	if(podVals->terminalCommand == terminalArm){
 		podVals->terminalCommand = terminalNone;
-		return 1;
+		return true;
 	}
-	return 0;
+	return false;
 }
 
 bool armingToArmed(PodValues* podVals){
@@ -34,8 +44,14 @@ bool armingToArmed(PodValues* podVals){
 }
 
 bool armedToFlight(PodValues* podVals){
-
-
+	TerminalCommands terminalState = podVals->terminalCommand;
+	BreakNodeStates nodeState = podVals->breakNodeS;
+	if(terminalState == terminalFlight /
+			& nodeState == bnFlight ){
+		podVals->terminalCommand = terminalNone;
+		podVals->motorS = drive;
+		return true;
+	}
 	return false;
 }
 
@@ -46,7 +62,10 @@ int runControlLoop(PodValues* podVals){
 	switch(podState){
 
 		case standby:{
-			if( checkFlags(podVals)){
+
+			BreakNodeStates bnAccepted[] = {bnBooting, bnBooting};
+
+			if( checkFlags(podVals) & nominalStates(podVals->breakNodeS, bnAccepted, 2)){
 				if(standbyToArming(podVals)){
 					podVals->podS = arming;
 				}
@@ -58,7 +77,10 @@ int runControlLoop(PodValues* podVals){
 			break;
 		}
 		case arming: {
-			if(checkFlags(podVals)){
+
+			BreakNodeStates bnAccepted[] = {bnArming, bnArmed};
+
+			if(checkFlags(podVals) & nominalStates(podVals->breakNodeS, bnAccepted, 2) ){
 				if(armingToArmed(podVals)){
 					podVals->podS = armed;
 				}
@@ -70,7 +92,10 @@ int runControlLoop(PodValues* podVals){
 			break;
 		}
 		case armed:{
-			if(checkFlags(podVals)){
+
+			BreakNodeStates bnAccepted[] = { bnArmed };
+
+			if(checkFlags(podVals & nominalStates(podVals->breakNodeS, bnAccepted, 1) )){
 				if(armedToFlight(podVals)){
 					podVals->podS = acceleration;
 				}
