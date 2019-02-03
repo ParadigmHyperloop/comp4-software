5263c4600a0f827f073f755b5207e0503073bf66
Jonathan Taylor <jttaylor@mun.ca>
2019-02-02 21:56:50 -0330

    Fixed variables spelling and moved method definitions from .ino to a .cpp

---
+++ OnPod/Node/BrakeNode/BrakeNode.cpp
@@ -0,0 +1,31 @@
+#include "BrakeNodeStates.h"
+
+void BrakeNodeState::TransitionToNextState() {
+    if (state_==State::BOOT) {
+        CheckBoot();
+    }
+    else if (state_==State::STANDBY) {
+        CheckStandby();
+    }
+    else if (state_==Staet::ARMING) {
+        CheckArming();
+    }
+    else if (state_==State::ARMED) {
+        CheckArmed();
+    }
+    else if (state_==State::FLIGHT) {
+        CheckFlight();
+    }
+    else if (state_==State::BRAKING) {
+        CheckBraking();
+    }
+    else if (state_==State::VENTING) {
+        CheckVenting();
+    }
+    else if (state_==State::RETRIEVAL) {
+        CheckRetrieval();
+    }
+    else if (state_==State::ERROR) {
+        CheckError();
+    }
+}
