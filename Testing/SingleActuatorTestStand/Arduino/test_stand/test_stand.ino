5b60130e1830c682bb1e3e761800b341c55c3d03
Shane Williams <shane@Shanes-MacBook-Pro-148.local>
2019-02-19 22:22:50 -0330

    Add comments to test_stand.ino

--- Testing/SingleActuatorTestStand/Arduino/test_stand/test_stand.ino
+++ Testing/SingleActuatorTestStand/Arduino/test_stand/test_stand.ino
@@ -4,17 +4,17 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-bool solenoid_driven;
-
 void loop() {
   if (Serial.available() > 0) {
     char command = Serial.read();
     Serial.println(command);
     if (command - '0' == 0 || command - '0' == 1) {
-      solenoid_driven = command - '0';
+      bool solenoid_driven = command - '0';
+      // simulate activating the solenoid with an LED
       digitalWrite(LED_BUILTIN, solenoid_driven);
     }
   }
+  // simulate sensor values with random integers
   float tank_pressure = random(1, 1000);
   float piston_pressure = random(1, 1000);
   float temperature = random(1, 1000);
