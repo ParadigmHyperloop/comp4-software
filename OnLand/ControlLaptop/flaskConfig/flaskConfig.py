484c031758c865ac53af0929560d2e8be0078154
Andrew Nash <anash@mun.ca>
2019-02-27 23:59:40 -0330

    working config save/load

--- OnLand/ControlLaptop/flaskConfig/flaskConfig.py
+++ OnLand/ControlLaptop/flaskConfig/flaskConfig.py
@@ -1,6 +1,24 @@
+import json
+
+
 def read_config():
-    pass
+    with open('flaskConfig.txt', 'r') as infile:
+        config = json.load(infile)
+
+    return config
 
 
-def write_config():
-    pass
+def write_config(config):
+    with open('flaskConfig.txt', 'w') as outfile:
+        json.dump(config, outfile)
+
+
+if '__main__':
+    config = dict()
+    config['key1'] = 'value1'
+    config['key2'] = 'value2'
+    config['key3'] = ['value3.1', 'value3.2', 'value3.3']
+    config['key4'] = 'value4'
+
+    write_config(config)
+    assert(read_config() == config)
