af2500ecd4a6193cb2a9942ae705533a3f132407
Patrick Costello <patrcostello@gmail.com>
2019-03-01 11:40:41 -0330

    updated, error existing
    when querying data error:
    
    influxdb.exceptions.InfluxDBClientError: 400: {"error":"error parsing query: found -, expected ; at line 1, char 24"}
    
    been trying to find my error but not having any luck with it, all that is remaining from comments on pull request

--- Examples/PythonToInflux/database.py
+++ Examples/PythonToInflux/database.py
@@ -7,20 +7,26 @@ class Database:
         self.host = host
         self.port = port
         self.client = InfluxDBClient(self.host, self.port)
-        self.client.create_database(dbName)
-        self.client.switch_database(dbName)
+        self.dbName = dbName
 
-    def log(self, name, data):
+        try:
+            self.client.create_database(dbName)
+            print("Database " + dbName + " created...")
+        except InfluxDBClientError:
+            self.client.drop_database(dbName)
+            self.client.create_database(dbName)
+
+    def log(self, measurement, data):
         row = [
             {
-                "measurement": name,
+                "measurement": measurement,
                 "time": datetime.datetime.now(),
                 "fields": {
                     "reading": data
                 }
             }
         ]
-        self.client.write_points(row)
+        self.client.write_points(row, database=self.dbName)
 
     def close(self):
         self.client.close()
--- Examples/PythonToInflux/main.py
+++ Examples/PythonToInflux/main.py
@@ -3,17 +3,27 @@ import math
 from random import randint
 
 def main():
-    someDatabase = Database("localhost", 8086, "classtest")
+    someDatabase = Database("localhost", 8086, "pod-data")
+    someDatabase.client.create_retention_policy('some-data', '3d', 3, "pod-data", default=True)
 
     print("Press enter to log data...")
     while True:
+        # Exit test with q
         if input() == "q":
-            someDatabase.close()
-            print("Test finished")
+
+            # query = "SELECT time FROM sensor-data"
+            # result = someDatabase.client.query(query)
+            # print(result)
+
+            someDatabase.client.close()
+            print("\nTest finished\n")
             break;
+
+        # Input some random integer into sensor_data
         value = randint(0,9999)
-        someDatabase.log("sensor_data", value)
-        print("Value of sensor_date value " + str(value))
+        measurement = "sensor_data"
+        someDatabase.log(measurement, value)
+        print("Value of " + measurement + " value " + str(value))
 
 
 main()
