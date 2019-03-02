c7ef61229ae4e1d3c4f4307e5f63feec44b9677d
Shane Williams <shane@Shanes-MacBook-Pro-544.local>
2019-03-02 18:46:30 -0330

    Add custom SPI settings

--- OnPod/Node/Examples/ADC/main.cpp
+++ OnPod/Node/Examples/ADC/main.cpp
@@ -2,6 +2,7 @@
 #include <SPI.h>
 
 SPIClass adcSPI (&PERIPH_SPI1, MISO1, MOSI1, SCK1, PAD_SPI1_TX, PAD_SPI1_RX);
+SPISettings adcSPISettings(20000000, MSBFIRST, SPI_MODE0)
 
 const uint16_t CH7_READ = 4992;
 
@@ -11,9 +12,11 @@ void setup() {
 }
 
 void loop() {
+    adcSPI.beginTransaction(adcSPISettings)
     digitalWrite(PIN_SPI1_SS, LOW);
     uint16_t received_data = adcSPI.transfer(CH7_READ);
-    digitalWrite(PIN_SPI1_SS, LOW);
+    digitalWrite(PIN_SPI1_SS, HIGH);
+    adcSPI.endTransaction()
     Serial.println(received_data);
     delay(500);
 }
