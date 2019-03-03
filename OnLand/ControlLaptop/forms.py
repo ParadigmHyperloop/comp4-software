6ba75004cbda2c063cba6c74a5024b836d0d3abc
mohamedb <mmb186@munca>
2019-03-02 22:19:32 -0330

    Updated send config UI

--- OnLand/ControlLaptop/forms.py
+++ OnLand/ControlLaptop/forms.py
@@ -4,14 +4,14 @@ from wtforms.validators import DataRequired
 
 
 class FlightConfigurationForm(FlaskForm):
-    air_resistance = StringField('Ait Resistance (units)',
+    air_resistance = StringField('Air Resistance (units)',
                                  validators=[DataRequired("Air Resistance")])
     motor_speed = StringField('Motor Speed (units)',
                               validators=[DataRequired("Motor Speed")])
     tube_pressure = StringField('Tube Pressure (units)',
                                 validators=[DataRequired("Tube Pressure")])
-    watchdog_timeout = StringField('Watchdog Timout (ms)',
+    watchdog_timeout = StringField('Watchdog Timeout (ms)',
                                    validators=[DataRequired("Watchdog Timer")])
-    pod_ip = StringField('Pod address (IP)',
+    pod_ip = StringField('Pod Address (IP)',
                          validators=[DataRequired("Pod Address")])
     submit = SubmitField('Send Configuration')
