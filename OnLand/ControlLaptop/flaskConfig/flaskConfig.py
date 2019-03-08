c43a09ea86ce31d0826a3f4019a4de10567dc96c
Andrew Nash <anash@mun.ca>
2019-03-08 17:55:50 -0330

    facade update

--- OnLand/ControlLaptop/flaskConfig/flaskConfig.py
+++ OnLand/ControlLaptop/flaskConfig/flaskConfig.py
@@ -1,54 +1,62 @@
 import json
 
-
-# TODO: Add facade classs
-def read_config():
-    """
-    pre - assumes a config.json exists
-    returns - dict of json config
-    """
+# TODO: add db stuff in future?
+class FlaskConfig:
     
-    # Open config and read JSON
-    with open('flaskConfig.json', 'r') as infile:
-        config = json.load(infile)
-
-    return config
-
-
-def new_config(config):
-    """
-    config - dict of config options to be written to JSON file
+    def __init__(self, json, db):
+        self.json = json
+        self.db = db
     
-     - Ignores current config and overwrites it entirely with config provided
-    """
-                    
-    # Dump to JSON file
-    with open('flaskConfig.json', 'w') as outfile:
-        json.dump(config, outfile)
-
-
-def update_config(config):
-    """
-    pre - assumes a config.json exists
-    config - dict of config options to be written to JSON file
     
-     - Updates current config values to match config provided
-     - Appends any new values to the config
-     - Does not alter values in the config.json that are not contained 
-       in input config
-    """
+    def read_config(self):
+        """
+        pre - assumes a config.json exists
+        returns - dict of json config
+        """
+        
+        # Open config and read JSON
+        with open('flaskConfig.json', 'r') as infile:
+            config = json.load(infile)
     
-    # Load current config
-    current_config = read_config()
+        return config
     
-    # Append new & overlaping config values to current config
-    for new_key, new_val in config.items():
-        current_config[new_key] = new_val
-                    
-    # Dump back to JSON file
-    with open('flaskConfig.json', 'w') as outfile:
-        json.dump(config, outfile)
-
+    
+    def new_config(self, config):
+        """
+        config - dict of config options to be written to JSON file
+        
+         - Ignores current config and overwrites it entirely with config provided
+        """
+                        
+        # Dump to JSON file
+        with open('flaskConfig.json', 'w') as outfile:
+            json.dump(config, outfile)
+    
+    
+    def update_config(self, config):
+        """
+        pre - assumes a config.json exists
+        config - dict of config options to be written to JSON file
+        
+         - Updates current config values to match config provided
+         - Appends any new values to the config
+         - Does not alter values in the config.json that are not contained 
+           in input config
+        """
+        
+        # Load current config
+        current_config = self.read_config()
+        
+        # Append new & overlaping config values to current config
+        for new_key, new_val in config.items():
+            current_config[new_key] = new_val
+                        
+        # Dump back to JSON file
+        with open('flaskConfig.json', 'w') as outfile:
+            json.dump(config, outfile)
+            
+    json = False
+    db = False
 
 if '__main__':
     config = dict()
@@ -57,13 +65,16 @@ if '__main__':
     config['key3'] = ['value3.1', 'value3.2', 'value3.3']
     config['key4'] = 'value4'
 
-    new_config(config)
-    assert(read_config() == config)
+    # test new_config()
+    local_config = FlaskConfig()
+    local_config.new_config(config)
+    assert(local_config.read_config() == config)
     
-    old_config = read_config()
+    # test update_config
+    old_config = local_config.read_config()
     config['key4'] = 'value4.0'
     config['key5'] = 'value5'
-    update_config(config)
-    assert(read_config() == config)
-    assert(read_config()['key4'] != old_config['key4'])
-    assert(read_config()['key3'] == old_config['key3'])
+    local_config.update_config(config)
+    assert(local_config.read_config() == config)
+    assert(local_config.read_config()['key4'] != old_config['key4'])
+    assert(local_config.read_config()['key3'] == old_config['key3'])
