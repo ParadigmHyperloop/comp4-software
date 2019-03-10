38aa3c4980a09aac1f2202692f35172b7d96fa7b
mohamedb <mmb186@munca>
2019-03-10 00:49:14 -0330

    PodCommunicator to send commands to POD.

--- OnLand/ControlLaptop/SocketController.py
+++ OnLand/ControlLaptop/SocketController.py
@@ -1,43 +1,42 @@
 import socket
 
-from LocalStorage.ConfigurationSotrage import LocalStorage
+from flask import jsonify
 
 
 class PodConstants:
-    POD_COMMAND_PORT = 9999
+    POD_COMMAND_PORT = 65432
+    POD_ADDRESS = '127.0.0.1'
     TEST_JSON_COMMAND_DICTIONARY = {'command': 2, "other_commands": 'test'}
 
 
 class PodCommunicator:
     _pod_communicator_instance = None
     _pod_address = None
+    _pod_port = None
     _command_socket = None
     _queue = None
 
-    def __init__(self,
-                 pod_address=LocalStorage.get_default_configuration()['pod_address']):
+    def __init__(self, pod_address=PodConstants.POD_ADDRESS,
+                 pod_port=PodConstants.POD_COMMAND_PORT):
         """ Virtual Private Constructor"""
         if PodCommunicator._pod_communicator_instance is not None:
             raise Exception("Pod-Communicator Instance Already exists")
         else:
             PodCommunicator._pod_communicator_instance = self
-            self.pod_address = pod_address
+            self._pod_address = pod_address
+            self._pod_port = pod_port
             self._connect_to_pod()
 
     def _connect_to_pod(self):
-        _command_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._command_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            _command_socket.connect(self._pod_address, PodConstants._POD_COMMAND_PORT)
-
-            received = _command_socket.recv(1024)
+            self._command_socket.connect((self._pod_address, PodConstants.POD_COMMAND_PORT))
         finally:
-            pass
+            print('Connected...')
 
-    def run(self):
-        pass
-
-    def _send_command(self, json_command):
-        pass
+    def _send_command(self, command):
+        self._command_socket.sendall(jsonify(command=command))
+        data = self._command_socket.recv(1024)
 
     @staticmethod
     def get_pod_communicator():
@@ -46,7 +45,7 @@ class PodCommunicator:
             return PodCommunicator._pod_communicator_instance
 
     def update_pod_address(self, new_address):
-        self.pod_address = new_address
+        self._pod_address = new_address
 
     def get_pod_address(self):
         return self._pod_address
