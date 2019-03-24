3573c21d0ffb7f14b2baed185303010bcd0ed29f
Mark Duffett <mwd467@mun.ca>
2019-03-23 21:50:22 -0230

    rear node main now parsing rx and building tx msg, updates to nodeStructs.h and NodeEthernet

--- OnPod/Node/includes/NodeEthernet.h
+++ OnPod/Node/includes/NodeEthernet.h
@@ -1,5 +1,7 @@
-#ifndef ETHERNET_H
-#define ETHERNET_H
+#ifndef NODEETHERNET_H
+#define NODEETHERNET_H
+
+#include "NodeStructs.h"
 
 class UDPClass {
 private:
@@ -8,15 +10,20 @@ private:
     const uint16_t NODE_PORT;
     const uint8_t NODE_TYPE;
     EthernetUDP Udp;
+    uint32_t rxPacketNum = 0;
+    uint32_t txPacketNum = 1;
+
 public:
     UDPClass(uint8_t ETHERNET_SS_PIN, IPAddress NODE_IP,
              uint16_t NODE_PORT, uint8_t NODE_TYPE);
     void init();
     bool readPacket();
     bool sendPacket(IPAddress iDestinationIP, uint16_t iDestinationPort);
+    void parseRearPacket();
     // UDP_TX_PACKET_MAX_SIZE is 24B - this can be made much larger
     char iPacketRecvBuffer[UDP_TX_PACKET_MAX_SIZE];
     char iPacketSendBuffer[UDP_TX_PACKET_MAX_SIZE];
+    uint32_t getTxPacketNum(){return txPacketNum;};
 };
 
 #endif
