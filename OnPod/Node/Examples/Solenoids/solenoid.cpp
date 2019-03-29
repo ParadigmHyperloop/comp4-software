b716a1840d049e911f39357f9156e36934e23be1
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-03-29 16:56:12 -0230

    Add DRV8806 class

---
+++ OnPod/Node/Examples/Solenoids/solenoid.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include "solenoid.h"
+
+DRV8806::DRV8806(uint8_t PIN_LATCH, uint8_t PIN_DOUT, uint8_t PIN_DIN, uint8_t PIN_SCLK) :
+    PIN_LATCH(PIN_LATCH), PIN_DOUT(PIN_DOUT),
+    PIN_DIN(PIN_DIN), PIN_SCLK(PIN_SCLK)
+    {}
+
+void DRV8806::init() {
+    pinMode(PIN_SCLK, OUTPUT);
+    pinMode(PIN_LATCH, OUTPUT);
+    pinMode(PIN_DOUT, OUTPUT);
+    pinMode(PIN_DIN, INPUT);
+    digitalWrite(PIN_SCLK, LOW);
+    digitalWrite(PIN_LATCH, HIGH);
+    digitalWrite(PIN_DOUT, LOW);
+}
+
+void DRV8806::updateSolenoids() {
+    digitalWrite(PIN_LATCH, LOW);
+    digitalWrite(PIN_SCLK, LOW);
+    delayMicroseconds(10);
+
+    for (uint8_t i = 0; i < 8; i++) {
+        uint8_t iSolenoid = 7 - i;
+        bool bState = (iActiveSolenoids >> iSolenoid) & 1;
+        digitalWrite(PIN_DOUT, bState);
+        delayMicroseconds(1);
+        digitalWrite(PIN_SCLK, HIGH);
+        delayMicroseconds(1);
+        digitalWrite(PIN_SCLK, LOW);
+        delayMicroseconds(1);
+    }
+    digitalWrite(PIN_LATCH, HIGH);
+    Serial.println(iActiveSolenoids, BIN);
+}
+
+void DRV8806::enableSolenoid(uint8_t iSolenoid) {
+    iActiveSolenoids |= (1 << iSolenoid);
+}
+
+void DRV8806::disableSolenoid(uint8_t iSolenoid) {
+    iActiveSolenoids &= ~(1 << iSolenoid);
+}
---
+++ OnPod/Node/Examples/Solenoids/solenoid.h
@@ -0,0 +1,20 @@
+#ifndef SOLENOID_H
+#define SOLENOID_H
+
+class DRV8806 {
+private:
+    const uint8_t PIN_LATCH;
+    const uint8_t PIN_DOUT;
+    const uint8_t PIN_DIN;
+    const uint8_t PIN_SCLK;
+public:
+    uint8_t iActiveSolenoids = 0;
+
+    DRV8806(uint8_t PIN_LATCH, uint8_t PIN_DOUT, uint8_t PIN_DIN, uint8_t PIN_SCLK);
+    void init();
+    void updateSolenoids();
+    void enableSolenoid(uint8_t iSolenoids);
+    void disableSolenoid(uint8_t iSolenoids);
+};
+
+#endif
