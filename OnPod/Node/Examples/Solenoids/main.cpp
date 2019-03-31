85344573b0e6d6f18a43dcddf6d041e117f1efe4
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-03-31 17:57:51 -0230

    Change driver pins

--- OnPod/Node/Examples/Solenoids/main.cpp
+++ OnPod/Node/Examples/Solenoids/main.cpp
@@ -1,21 +1,18 @@
 #include <Arduino.h>
 #include "solenoid.h"
 
-DRV8806 solenoidDriver(SS2, MOSI2, MISO2, SCK2);
+DRV8806 solenoidDriver(SOL_CS, SOL_DOUT, SOL_DIN, SOL_CLK);
 
 void setup() {
-    Serial.begin(9600);
     solenoidDriver.init();
 }
 
 void loop() {
     solenoidDriver.enableSolenoid(5);
-    solenoidDriver.enableSolenoid(7);
     solenoidDriver.updateSolenoids();
     delay(1000);
 
     solenoidDriver.disableSolenoid(5);
-    solenoidDriver.disableSolenoid(7);
     solenoidDriver.updateSolenoids();
     delay(1000);
 }
