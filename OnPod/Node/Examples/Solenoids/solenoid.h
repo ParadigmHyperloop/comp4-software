6a2c47ac527e37e2dbd3e7b505f7e587b7bf9f08
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-03-31 18:09:16 -0230

    Fix whitespace

--- OnPod/Node/Examples/Solenoids/solenoid.h
+++ OnPod/Node/Examples/Solenoids/solenoid.h
@@ -7,9 +7,8 @@ private:
     const uint8_t PIN_DOUT;
     const uint8_t PIN_DIN;
     const uint8_t PIN_SCLK;
-public:
     uint8_t iActiveSolenoids = 0;
-
+public:
     DRV8806(uint8_t PIN_LATCH, uint8_t PIN_DOUT, uint8_t PIN_DIN, uint8_t PIN_SCLK);
     void init();
     void updateSolenoids();
