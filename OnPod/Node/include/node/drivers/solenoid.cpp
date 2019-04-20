65009a766eb1440701e78ee4e42ac67dd4453d3d
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-04-19 18:05:00 -0700

    Add state attribute to solenoid class

--- OnPod/Node/include/node/drivers/solenoid.cpp
+++ OnPod/Node/include/node/drivers/solenoid.cpp
@@ -8,9 +8,11 @@ Solenoid::Solenoid(DRV8806 *driver, uint8_t uSolenoidChannel) :
 void Solenoid::enable() {
     driver->enableSolenoid(uSolenoidChannel);
     driver->updateSolenoids();
+    bState = 1;
 }
 
 void Solenoid::disable() {
     driver->disableSolenoid(uSolenoidChannel);
     driver->updateSolenoids();
+    bState = 0;
 }
--- OnPod/Node/include/node/drivers/solenoid.h
+++ OnPod/Node/include/node/drivers/solenoid.h
@@ -9,6 +9,7 @@ private:
     uint8_t uSolenoidChannel;
 public:
     Solenoid(DRV8806 *driver, uint8_t uSolenoidChannel);
+    bool bState = 0;
     void enable();
     void disable();
 };
