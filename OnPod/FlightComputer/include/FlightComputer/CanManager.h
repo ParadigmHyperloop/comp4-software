bb1803f3f857bdad62d5291176bf1ae0402710db
Unknown <liamwaghorn@gmail.com>
2019-05-10 17:50:19 -0230

    Template function for parsing

--- OnPod/FlightComputer/include/FlightComputer/CanManager.h
+++ OnPod/FlightComputer/include/FlightComputer/CanManager.h
@@ -1,12 +1,42 @@
-//
-// Created by liam on 09/05/19.
-//
-
 #ifndef FLIGHTCOMPUTER_CANMANAGER_H
 #define FLIGHTCOMPUTER_CANMANAGER_H
 
 #endif //FLIGHTCOMPUTER_CANMANAGER_H
 
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <endian.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <iomanip>
+#include <cerrno>
+#include <csignal>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+
+#include "FlightComputer/Network.h"
+#include "FlightComputer/Common.h"
+#include "EasyLogger/easylogging++.h"
+
 int CanThread(Pod);
 
 unsigned int convertBytesToInt(int, int);
+
+
+template <class T>
+void extractCanValue(__uint8 data[], int iByteIndex[], int iNumberOfBytes ,T* tStoreLocation, T tConversion){
+    unsigned int uiValue;
+    T tConverted;
+    std::stringstream strStream;
+    strStream << std::hex;
+
+    for(int i = 0 ; i< iNumberOfBytes ; i++){
+        strStream << data[iByteIndex[i]];
+    }
+    strStream >> uiValue;
+    tConverted = uiValue/tConversion;
+    *tStoreLocation = tConverted;
+}
+
--- OnPod/FlightComputer/src/CanManager.cpp
+++ OnPod/FlightComputer/src/CanManager.cpp
@@ -1,20 +1,7 @@
-#include "FlightComputer/Network.h"
-#include "FlightComputer/Common.h"
-#include "EasyLogger/easylogging++.h"
+
 #include "FlightComputer/CanManager.h"
 
-#include <linux/can.h>
-#include <linux/can/raw.h>
-#include <endian.h>
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <iomanip>
-#include <cerrno>
-#include <csignal>
-#include <cstdint>
-#include <cstdio>
-#include <cstring>
+
 
 
 void processFrame(const struct canfd_frame &frame, Pod pod) {
