aabefaa6dd56efe2b540b508ce4616ca5b21b0cc
mohamedb <mmb186@munca>
2019-05-10 18:44:16 -0400

    WIP - merge

---
+++ OnPod/FlightComputer/src/Models/FlightConfig.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MohamedB on 2019-05-10.
+//
+
+#include "FlightConfig.h"
---
+++ OnPod/FlightComputer/src/Models/FlightConfig.h
@@ -0,0 +1,14 @@
+//
+// Created by MohamedB on 2019-05-10.
+//
+
+#ifndef FLIGHTCOMPUTER_FLIGHTCONFIG_H
+#define FLIGHTCOMPUTER_FLIGHTCONFIG_H
+
+
+class FlightConfig {
+
+};
+
+
+#endif //FLIGHTCOMPUTER_FLIGHTCONFIG_H
