4b9f985239812d193c2678c7f1ffcdb465e03876
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-05-18 15:14:18 -0230

    Move IPs and ports into pod_internal_network.h

--- OnPod/Node/RearNode/main.cpp
+++ OnPod/Node/RearNode/main.cpp
@@ -6,7 +6,7 @@
 #include "Sodaq_wdt.h"
 #include "Timer.h"
 
-#include "NodeStructs.h"
+#include "../../pod_internal_network.h"
 #include "Paradigm.pb.h"
 
 #include "drivers/node_ethernet.h"
@@ -15,19 +15,16 @@
 
 const NodeType NODE_TYPE = REAR;
 
-// timer
 Timer txTimer;
-const uint32_t TX_INTERVAL_MS = 50;
 
 // sensors
 SPIClass adcSPI (&PERIPH_SPI1, MISO1, SCK1, MOSI1, PAD_SPI1_TX, PAD_SPI1_RX);
 ADS7953 adc(adcSPI);
-/*
+
 typeKThermo thermo(&adc, 0);
 
 // comms
-UDPParams IPPara;  // keep IPs and ports in one location
-UDPClass udp(PIN_SPI_SS, IPAddress(IPPara.REAR_NODE_IP), IPPara.NODE_RX_PORT, NODE_TYPE);
+UDPClass udp(PIN_SPI_SS, REAR_NODE_IP, REAR_NODE_PORT, NODE_TYPE);
 RearNodeToFc pRearNodeTelemetry = RearNodeToFc_init_default;  // protobuf message
 
 void sendToFlightComputer(void*) {
@@ -35,18 +32,17 @@ void sendToFlightComputer(void*) {
     pb_ostream_t outStream = pb_ostream_from_buffer(udp.uSendBuffer, sizeof(udp.uSendBuffer));
     // encode the message object and store it in the UDP buffer
     pb_encode(&outStream, RearNodeToFc_fields, &pRearNodeTelemetry);
-    udp.sendPacket(IPAddress(IPPara.BBB_IP), IPPara.BBB_PORT, outStream.bytes_written);
+    udp.sendPacket(FC_IP, FC_PORT, outStream.bytes_written);
 }
-*/
+
 void setup() {
-    //udp.init();
+    udp.init();
     adc.init();
 
-    //txTimer.every(TX_INTERVAL_MS, &sendToFlightComputer, (void*)0);
+    txTimer.every(REAR_NODE_TO_FC_INTERVAL, &sendToFlightComputer, (void*)0);
 }
 
 void loop() {
-    /*
 	adc.readActiveChannels();  // update sensor data
     // store sensor data in the protobuf message object
     pRearNodeTelemetry.coolantTemperature = thermo.read();
@@ -55,7 +51,4 @@ void loop() {
     pRearNodeTelemetry.coolantPressure1 = 0;
     pRearNodeTelemetry.tubePressure = 0;
     txTimer.update();  // check to see if it's time to send another packet
-*/
-Serial.println(adc.readSingleChannel(11));
-delay(200);
 }
---
+++ OnPod/pod_internal_network.h
@@ -0,0 +1,32 @@
+#ifndef POD_INTERNAL_NETWORK
+#define POD_INTERNAL_NETWORK
+
+// IP addresses
+const uint8_t FC_IP[4] = {192, 168, 0, 10};
+const uint8_t REAR_NODE_IP[4] = {192, 168, 0, 20};
+const uint8_t BRAKE_NODE_IP[4] = {192, 168, 0, 21};
+const uint8_t LVDC_NODE_IP[4] = {192, 168, 0, 22};
+const uint8_t ENCLOSURE_ARDUINO_IP[4] = {192, 168, 0, 23};
+
+// IP ports
+const uint16_t FC_PORT = 5000;
+const uint16_t REAR_NODE_PORT = 5000;
+const uint16_t BRAKE_NODE_PORT = 5000;
+const uint16_t LVDC_NODE_PORT = 5000;
+const uint16_t ENCLOSURE_ARDUINO_PORT = 5000;
+
+// send intervals
+const uint16_t FC_TO_BRAKE_NODE_INTERVAL = 50;
+const uint16_t REAR_NODE_TO_FC_INTERVAL = 50;
+const uint16_t BRAKE_NODE_TO_FC_INTERVAL = 50;
+const uint16_t LVDC_NODE_TO_FC_INTERVAL = 50;
+const uint16_t ENCLOSURE_ARDUINO_TO_FC_INTERVAL = 50;
+
+enum NodeType {
+	RESERVED = 0,
+	BRAKE = 1,
+	LVDC = 2,
+	REAR = 3
+};
+
+#endif
