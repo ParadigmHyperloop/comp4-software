f2409a2f38a83b09fed74aa12ae1d972ae013d1b
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-05-24 15:33:51 -0230

    Remove rear node IP config

--- OnPod/pod_internal_network.h
+++ OnPod/pod_internal_network.h
@@ -3,21 +3,18 @@
 
 // IP addresses
 const uint8_t FC_IP[4] = {192, 168, 0, 10};
-const uint8_t REAR_NODE_IP[4] = {192, 168, 0, 20};
-const uint8_t BRAKE_NODE_IP[4] = {192, 168, 0, 21};
-const uint8_t LVDC_NODE_IP[4] = {192, 168, 0, 22};
-const uint8_t ENCLOSURE_ARDUINO_IP[4] = {192, 168, 0, 23};
+const uint8_t BRAKE_NODE_IP[4] = {192, 168, 0, 20};
+const uint8_t LVDC_NODE_IP[4] = {192, 168, 0, 21};
+const uint8_t ENCLOSURE_ARDUINO_IP[4] = {192, 168, 0, 22};
 
 // IP ports
-const uint16_t FC_PORT = 5000;
-const uint16_t REAR_NODE_PORT = 5000;
-const uint16_t BRAKE_NODE_PORT = 5000;
-const uint16_t LVDC_NODE_PORT = 5000;
-const uint16_t ENCLOSURE_ARDUINO_PORT = 5000;
+const uint16_t FC_PORT = 5555;
+const uint16_t BRAKE_NODE_PORT = 5555;
+const uint16_t LVDC_NODE_PORT = 5555;
+const uint16_t ENCLOSURE_ARDUINO_PORT = 5555;
 
 // send intervals
 const uint16_t FC_TO_BRAKE_NODE_INTERVAL = 50;
-const uint16_t REAR_NODE_TO_FC_INTERVAL = 50;
 const uint16_t BRAKE_NODE_TO_FC_INTERVAL = 50;
 const uint16_t LVDC_NODE_TO_FC_INTERVAL = 50;
 const uint16_t ENCLOSURE_ARDUINO_TO_FC_INTERVAL = 50;
@@ -26,7 +23,6 @@ enum NodeType {
 	RESERVED = 0,
 	BRAKE = 1,
 	LVDC = 2,
-	REAR = 3
 };
 
 #endif
