14d8a6a54028b23f977fdd5e1270d40dba766525
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-05-25 15:00:19 -0230

    Add solenoid NO/NC logic

--- OnPod/Node/include/drivers/solenoid.h
+++ OnPod/Node/include/drivers/solenoid.h
@@ -3,15 +3,21 @@
 
 #include "drivers/solenoid_driver_DRV8806.h"
 
+enum SolenoidType {
+    NATURALLY_CLOSED = 0,
+    NATURALLY_OPEN = 1,
+};
+
 class Solenoid {
 private:
     DRV8806 *driver;
     uint8_t uSolenoidChannel;
+    SolenoidType eSolenoidType;
 public:
-    Solenoid(DRV8806 *driver, uint8_t uSolenoidChannel);
+    Solenoid(DRV8806 *driver, uint8_t uSolenoidChannel, SolenoidType eSolenoidType);
     bool bState = 0;
-    void enable();
-    void disable();
+    void open();
+    void close();
 };
 
 #endif
