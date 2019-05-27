2718d831246d141c66376bb00c9f9cacf063e470
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-05-27 19:05:49 -0230

    Add enclosure node boilerplate code

--- OnPod/Node/EnclosureNode/main.cpp
+++ OnPod/Node/EnclosureNode/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <pb_encode.h>
 #include <pb_decode.h>
-#include <Wire.h>
 
 #include "Timer.h"
 
@@ -10,44 +9,35 @@
 
 #include "drivers/node_ethernet.h"
 
-
-
 const NodeType NODE_TYPE = ENCLOSURE;
 
 Timer txTimer;
 
 // comms
 UDPClass udp(PIN_SPI_SS, ENCLOSURE_NODE_IP, ENCLOSURE_NODE_PORT, NODE_TYPE);
-RearNodeToFc pRearNodeTelemetry = RearNodeToFc_init_default;  // protobuf message
+EnclosureNodeToFc pEnclosureNodeTelemetry = EnclosureNodeToFc_init_default;
 uint32_t txPacketNum = 0;
 
 void sendToFlightComputer(void*) {
-    pRearNodeTelemetry.packetNum = txPacketNum;
+    pEnclosureNodeTelemetry.packetNum = txPacketNum;
     // create an output stream that writes to the UDP buffer
     pb_ostream_t outStream = pb_ostream_from_buffer(
         udp.uSendBuffer, sizeof(udp.uSendBuffer)
     );
     // encode the message object and store it in the UDP buffer
-    pb_encode(&outStream, RearNodeToFc_fields, &pRearNodeTelemetry);
+    pb_encode(&outStream, EnclosureNodeToFc_fields, &pEnclosureNodeTelemetry);
     if (udp.sendPacket(FC_IP, FC_ENCLOSURE_NODE_PORT, outStream.bytes_written)) {
         txPacketNum++;
     }
-    Wire.begin();
 }
-
 
 void setup() {
     Serial.begin(9600);
     udp.init();
     txTimer.every(ENCLOSURE_NODE_TO_FC_INTERVAL, &sendToFlightComputer, (void*)0);
-    //Wire.begin();
 }
 
 void loop() {
-//    txTimer.update();  // check to see if it's time to send another packet
-    delay(500);
-    Wire.requestFrom(0x28 , 2);
-    uint16_t reading = Wire.read() << 8;
-    reading |= Wire.read();
-    Serial.println(reading);
+    txTimer.update();  // check to see if it's time to send another packet
+
 }
