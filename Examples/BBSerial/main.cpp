082874eda98e66374972203f261f238472c4eff7
Liam <liamwaghorn@gmail.com>
2019-06-27 18:03:57 -0230

    parsing for comma delimited payload

--- Examples/BBSerial/main.cpp
+++ Examples/BBSerial/main.cpp
@@ -21,78 +21,6 @@
 #include <vector>
 #include <sstream>
 
-void reading(int fd)
-{
-    char red[20];
-    memset(red, 0, sizeof(red));
-    int ret,n = 0;
-
-    int num=0;
-    while(1)
-    {
-        ret=read(fd,red, sizeof(red));
-        if(ret > 0){
-            std::string num(red);
-            std::cout<<num << std::endl;
-        }
-    }
-}
-
-int open_port(void)
-{
-    int serial_port,ret;
-    char red[88];
-    serial_port = open("/dev/ttyUSB1", O_RDONLY | O_NOCTTY);
-    printf("\nfunction name is  %s >>\n",__func__);
-    printf("returned fd is :%d\n",serial_port );
-    if (serial_port == -1)
-    {
-        perror("open_port: Unable to open /dev/ttyUSB0 - ");
-    }
-    // Create new termios struc, we call it 'tty' for convention
-    struct termios tty = {};
-    // Read in existing settings, and handle any error
-    if(tcgetattr(serial_port, &tty) != 0) {
-        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-    }
-
-    tty.c_cflag &= ~PARENB; /*CLEAR Parity Bit PARENB*/
-    tty.c_cflag |=  PARENB; /*SET   Parity Bit PARENB*/
-    tty.c_cflag &= ~PARENB;   // No Parity
-    tty.c_cflag &= ~CSTOPB; //Stop bits = 1
-    tty.c_cflag &= ~CSIZE; /* Clears the Mask       */
-    tty.c_cflag |=  CS8;   /* Set the data bits = 8 */
-    tty.c_cflag &= ~CRTSCTS;
-    tty.c_cflag |= CREAD | CLOCAL;
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-    tty.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
-
-    tty.c_cc[VMIN] = 100;
-    tty.c_cc[VTIME] = 0;
-// Set in/out baud rate to be 9600
-    cfsetispeed(&tty, B9600);
-    cfsetospeed(&tty, B9600);
-
-// Save tty settings, also checking for error
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-    }
-
-    return (serial_port);
-}
-
-/*
-int main()
-{
-    int k = open_port();
-    if(k < 0)
-        return 0;
-    reading(k);
-    return 0;
-}
-*/
-
 int main(void)
 {
     int fd;
@@ -101,7 +29,6 @@ int main(void)
         printf("\n Error! in Opening");
     else
         printf("\n Opened Successfully ");
-
     struct termios SerialPortSettings;
     tcgetattr(fd, &SerialPortSettings);
     cfsetispeed(&SerialPortSettings,B19200);
@@ -117,15 +44,15 @@ int main(void)
     SerialPortSettings.c_oflag &= ~OPOST;
     SerialPortSettings.c_cc[VMIN] = 0;
     SerialPortSettings.c_cc[VTIME] = 100;
-
     if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0) { /* Set the attributes to the termios structure*/
         printf("\n  ERROR ! in Setting attributes");
     }
     tcflush(fd, TCIFLUSH);
     char read_buffer[10] = {};
-    int  bytes_read = 0;
+    int  bytes_read, val_1, val_2 = 0;
     int total_bytes_read = 0;
-    std::stringstream data;
+    std::stringstream dataStream;
+    std::string data;
     while(1)
     {
         write (fd, "1", 1);
@@ -133,15 +60,20 @@ int main(void)
             bytes_read=read(fd,read_buffer, sizeof(read_buffer));
             if(bytes_read > 0){
                 total_bytes_read += bytes_read;
-                data << read_buffer;
+                dataStream << read_buffer;
                 memset(&read_buffer,0, bytes_read);
             } else{
                 std::cout<<"Expired"<< std::endl;
             }
         }
-        std::cout<<data.str()<< std::endl;
-        data.str(std::string());
+        std::getline(dataStream, data, ',');
+        val_1 = std::stoi(data);
+        dataStream >> val_2;
+
+        dataStream.str(std::string());
         total_bytes_read = 0;
+
+        std::cout<<"Val1 : "<< val_1<< " Val2 : " << val_2<< std::endl;
     }
-    close(fd); /* Close the serial port */
+    close(fd); 
 }
