20e867bb67dbde7c07acb0afb84e4c15885b4992
Mohamed B <mmb186@mun.ca>
2019-07-08 23:24:02 -0400

    Implemented ThreadMonitoring Mechanism

---
+++ OnPod/FlightComputer/include/ThreadMonitorStatus.h
@@ -0,0 +1,14 @@
+
+
+#ifndef FLIGHTCOMPUTER_THREADMONITORSTATUS_H
+#define FLIGHTCOMPUTER_THREADMONITORSTATUS_H
+
+enum class ThreadMonitorStats
+{
+  Active,
+  Busy,
+  Non_Responsive,
+  Danger,
+};
+
+#endif //FLIGHTCOMPUTER_THREADMONITORSTATUS_H
---
+++ OnPod/FlightComputer/src/Threads/ThreadMonitor/ThreadMonitorWrapper.cpp
@@ -0,0 +1,19 @@
+#include "ThreadMonitorWrapper.h"
+
+
+ThreadMonitorWrapper::ThreadMonitorWrapper(ThreadMonitor* threadMonitor)
+:_threadMonitor(threadMonitor)
+{
+
+}
+
+ThreadMonitorStats ThreadMonitorWrapper::GetStatus()
+{
+  std::lock_guard<std::mutex> lck(*this->_mutex);
+  return this->_threadMonitor->GetStatus();
+}
+
+void ThreadMonitorWrapper::Feed() {
+  std::lock_guard<std::mutex> lck(*this->_mutex);
+  this->_threadMonitor->Feed();
+}
