a98d0a537c367a01bf87dd8d45a4b994fe027be0
Andrew Nash <33398082+andrewnash@users.noreply.github.com>
2019-07-16 04:16:19 -0230

    add inverter error parsing

--- OnLand/ControlLaptop/static/assets/js/SensorTable.js
+++ OnLand/ControlLaptop/static/assets/js/SensorTable.js
@@ -75,6 +75,7 @@ $( document ).ready(function() {
 socket.on('pod_telemetry', function (data) {
     let value, value_cell, value_name, current_state;
     data = JSON.parse(data);
+    console.log(data)
     const length = telemetry_value_elements.length;
 
     current_state = data['podState']
@@ -84,12 +85,17 @@ socket.on('pod_telemetry', function (data) {
 
         value_cell = telemetry_value_elements[index];
         value_name = value_cell.id;
+
         if(!data.hasOwnProperty(value_name)){
             console.log(value_name + " not found in packet")
             continue;
         }
         value = data[value_name];
         let $value_cell = $('#'+value_name);
+
+        if(value_name === 'inverterFaultBitHi' || value_name === 'inverterFaultBitLo' || value_name === 'hvFaultCode1' || value_name === 'hvFaultCode2') {
+            convertFaultCode(value_name, value)
+        }
 
         if(typeof(data[value_name]) == "boolean" || isNaN(data[value_name])){
             $value_cell.text(value)
@@ -108,5 +114,111 @@ socket.on('pod_telemetry', function (data) {
 });
 
 
+function convertFaultCode(value_name, value) {
+    if(value_name === 'inverterFaultBitLo') {
+        errors = converInverterFaultCodeLo(value)
+        console.error(`INVERTER FAULTS: ${errors}`)
+
+    }
+    if(value_name === 'inverterFaultBitLo') {
+        errors = converInverterFaultCodeHi(value)
+        console.error(`INVERTER FAULTS: ${errors}`)
+    }
+    else if(value_name === 'hvFaultCode1') {
+        console.log(value)
+    }
+    else if(value_name === 'hvFaultCode2') {
+        console.log(value)
+    }
+    else {
+        console.error(`COULD NOT CONVERT ERROR CODE FOR ${value_name}`)
+    }
+}
 
 
+function addErrorsToUpdatesTable(errors) {
+    // if error not in tabke add it
+    for (const error of errors) {
+        console.log(updatesTable)
+        var flag = 0
+
+        $('#events-table-body').find("tr").each(function () {
+            var td2 = $(this).find("td:eq(1)").text();
+            
+            if (td2 === error) { 
+                flag = 1; 
+            }
+        });
+        console.log(flag)
+        if (flag === 0) {
+            let row = `<tr>
+                      <td><i class="fa fa-info text-info"></i></td>
+                      <td>${error}</td>
+                    </tr>`;
+
+            $('#events-table-body').prepend(row);
+        }
+    }
+}
+
+
+function converInverterFaultCodeLo(value) {
+    let errors = []
+
+    if(value & 1) {
+        errors.push('Inverter: Motor Over-speed Fault')   
+    }
+    if(value & 2) {
+        errors.push('Inverter: Over-current Fault')
+    }
+    if(value & 4) {
+        errors.push('Inverter: Over-voltage Fault')
+    }
+    if(value & 8) {
+        errors.push('Inverter: Inverter Over-temperature Fault')
+    }
+    if(value & 10) {
+        errors.push('Inverter: Accelerator Input Shorted Fault')
+    }
+    if(value & 32) {
+        errors.push('Inverter: Accelerator Input Open Fault')   
+    }
+    if(value & 64) {
+        errors.push('Inverter: Direction Command Fault (Both directions active at the same time)')
+    }
+    if(value & 128) {
+        errors.push('Inverter: Inverter Response Time-out Fault')
+    }
+    if(value & 256) {
+        errors.push('Inverter: Hardware Desaturation Fault')
+    }
+    if(value & 512) {
+        errors.push('Inverter: Hardware Over-current Fault')
+    }
+    if(value & 1024) {
+        errors.push('Inverter: Under-voltage Fault')
+    }
+    if(value & 2048) {
+        errors.push('Inverter: CAN Command Message Lost Fault')
+    }
+    if(value & 4096) {
+        errors.push('Inverter: Motor Over-temperature Fault')
+    }
+
+    addErrorsToUpdatesTable(errors)
+    return errors
+}
+
+function converInverterFaultCodeHi(value) {
+    let errors = []
+
+    if(value & 1) {
+        errors.push('Inverter: Brake Input Shorted Fault')
+    }
+    if(value & 2) {
+        errors.push('Inverter: Brake Input Open Fault')
+    }
+
+    addErrorsToUpdatesTable(errors)
+    return errors
+}
