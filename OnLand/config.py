997eac657e06ff179e2448f254cd965cc4cdd1f0
Unknown <liamwaghorn@gmail.com>
2019-07-18 14:11:49 -0700

    lower frontend broadcast frequency

--- OnLand/config.py
+++ OnLand/config.py
@@ -20,7 +20,7 @@ UDP_RECV_BUFFER_SIZE = 50
 # Udp Telemetry Stream
 UDP_TELEM_TIMEOUT = 3  # How long to wait without receiving telemetry before deciding connection is lost
 UDP_TELEM_PORT = 7000  # Control Server Port to receive telemetry over
-TELEMETRY_BROADCAST_FREQUENCY = 500  # Minimum time between telemetry broadcasts
+TELEMETRY_BROADCAST_FREQUENCY = 1000  # Minimum time between telemetry broadcasts
 
 
 # SocketIO
